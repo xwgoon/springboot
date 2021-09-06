289c48de0401be6d4cace6a74323c460836db43f
huangxiaowei <1178124579@qq.com>
2021-09-06 18:32:59 +0800

    初始化

---
+++ web/src/main/java/com/exercise/springboot/web/Application.java
@@ -0,0 +1,10 @@
+package com.exercise.springboot.web;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
